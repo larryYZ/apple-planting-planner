eb5964c9a195b9ba3e291937364e48821c89b9e8
Larry Zhang <larryz@my.yorku.ca>
2022-08-29 11:59:08 -0400

    initial commit, set up background styling and overall structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ForestIcon from '@mui/icons-material/Forest';;
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <ForestIcon fontSize="large"/> Apple Planting Planner
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
